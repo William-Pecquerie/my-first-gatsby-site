0820f507946627e1d6f5e29b19dc088caaccb361
MSI-VAMPIRIC\William <william.pecquerie@gmail.com>
2022-03-23 12:39:43 +0100

    Finished Gatsby Tutorial Part 4

---
+++ src/pages/blog.js
@@ -0,0 +1,34 @@
+// Step 1: Import React
+import * as React from 'react'
+import { graphql } from 'gatsby'
+import Layout from '../components/layout'
+
+// Step 2: Define your component
+const BlogPage = ({ data }) => {
+  return (
+    <Layout pageTitle="My Blog Posts">
+      <ul>
+      {
+        data.allFile.nodes.map(node => (
+          <li key={node.name}>
+            {node.name}
+          </li>
+        ))
+      }
+      </ul>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query {
+    allFile {
+      nodes {
+        name
+      }
+    }
+  }
+`
+
+// Step 3: Export your component
+export default BlogPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,10 +8,13 @@ const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial on index.</p>
+      {//Adding image from web
+      }
       <StaticImage
         alt="Roronoa Zoro Oni-Giri pose"
         src="https://preview.redd.it/llvhem2yvjp71.jpg?width=960&crop=smart&auto=webp&s=e4d4b140df89badbad56d41985975fd62759182d"
       />
+      {/*Adding image from Desktop*/}
       <StaticImage
         alt="Roronoa Zoro Oni-Giri pose"
           src="../../../../../dragon_image_fond_ecran_0027_resize.jpg"
