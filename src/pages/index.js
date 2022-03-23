dce7362d2807185759fb8bf048fd22d04a2402c3
MSI-VAMPIRIC\William <william.pecquerie@gmail.com>
2022-03-23 11:04:58 +0100

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,21 @@
 // Step 1: Import React
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 // Step 2: Define your component
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial on index.</p>
+      <StaticImage
+        alt="Roronoa Zoro Oni-Giri pose"
+        src="https://preview.redd.it/llvhem2yvjp71.jpg?width=960&crop=smart&auto=webp&s=e4d4b140df89badbad56d41985975fd62759182d"
+      />
+      <StaticImage
+        alt="Roronoa Zoro Oni-Giri pose"
+          src="../../../../../dragon_image_fond_ecran_0027_resize.jpg"
+      />      
     </Layout>
   )
 }
